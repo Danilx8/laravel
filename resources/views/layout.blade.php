8e2136435ca5853a55145169ad13a1f924470ad5
Danilx8 <lugovskihdanil@yandex.ru>
2023-12-12 16:24:41 +0700

    add laravel mix to the project

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -3,7 +3,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" type="text/css" href="{{ asset('/css/welcome.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{ mix('/css/welcome.css')}}">
     <title>Document</title>
 </head>
 <body>
@@ -25,9 +25,14 @@
 
         </div>
     </header>
+    <div id="app">
+        <App/>
+    </div>
     @yield('content')
     <footer>
         <p>Луговских Данил Викторович 221-323</p>
     </footer>
 </body>
 </html>
+
+<script src="{{ mix('js/app.js') }}"></script>
